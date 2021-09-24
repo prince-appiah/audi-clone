fa36deef62ff6d6da00bc9ec3deb832f356a2431
Prince Appiah <pappiah00@gmail.com>
2021-09-24 12:16:18 +0000

    Added AudiWorld section

---
+++ src/components/AudiWorld.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const AudiWorld = () => {
+  return (
+    <section className="container px-5 py-20">
+      <div className="text-center px-52">
+        <h3 className="py-5 mb-10 text-5xl font-light">The world of Audi</h3>
+        <p className="text-xl font-light tracking-wide">
+          Discover Audi as a brand, company and employer on our international
+          website. Here you will find information about models and technologies.
+          Inspiring content, interesting backgrounds and fascinating moments –
+          digital, individual and authentic. Experience our vision of mobility
+          and let yourself be inspired.
+        </p>
+      </div>
+    </section>
+  );
+};
