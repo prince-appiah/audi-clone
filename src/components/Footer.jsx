114c9ff01ae358fb08e79ec86ce6daf560690150
Prince Appiah <pappiah00@gmail.com>
2021-10-15 21:27:56 +0000

    Add footer data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import data from "../data/footer";
+import data, { longTexts } from "../data/footer";
 import {
   FaFacebook,
   FaInstagram,
@@ -11,6 +11,7 @@ import {
 export const Footer = () => {
   // eslint-disable-next-line
   const [links, setLinks] = useState(data);
+  const [paragraphs, setParagraphs] = useState(longTexts);
   const socialIcons = [
     FaFacebook,
     FaInstagram,
@@ -19,19 +20,19 @@ export const Footer = () => {
     FaLinkedin,
   ];
   return (
-    <div className="text-white bg-black">
+    <div className="px-4 text-white bg-black">
       {/* social icons */}
-      <ul className="flex justify-end px-10 py-10">
+      <ul className="flex justify-end py-8">
         {socialIcons.map((Icon) => (
-          <li className="pr-10">{<Icon size="40" />}</li>
+          <li className="pr-6">{<Icon size="28" />}</li>
         ))}
       </ul>
       {/* links */}
       <div className="">
         {links.map((item) => (
-          <div className="" key={item.id}>
-            <h4 className="">{item.title}</h4>
-            <ul className="">
+          <div className="py-4" key={item.id}>
+            <h4 className="font-sans font-medium">{item.title}</h4>
+            <ul className="font-thin">
               {item.links.map((link) => (
                 <li className="" key={link}>
                   {link}
@@ -42,7 +43,27 @@ export const Footer = () => {
         ))}
       </div>
       {/* divider and copyright */}
+      <div className="flex flex-col mt-8 font-light">
+        <h4> &copy; AUDI AG. All rights reserved</h4>
+        <ul className="flex flex-wrap-reverse justify-between font-thin sm:flex-none sm:justify-start">
+          <div className="pt-4">
+            <li>Legal notice</li>
+            <li>Cookie Policy</li>
+          </div>
+          <div className="pt-4">
+            <li>Legal</li>
+            <li>Cookie Consent Optins</li>
+          </div>
+        </ul>
+      </div>
       {/* some long paragraph */}
+      <div className="py-14">
+        {longTexts.map((item) => (
+          <p key={item.id} className="py-4 font-thin text-justify">
+            {item.text}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
--- src/data/footer.js
+++ src/data/footer.js
@@ -1,26 +1,40 @@
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
 const data = [
   {
     id: uuidv4(),
-    title: 'Services',
-    links: ['myAudi', 'Press', 'Contact'],
+    title: "Services",
+    links: ["myAudi", "Press", "Contact"],
   },
   {
     id: uuidv4(),
-    title: 'Experience Audi',
+    title: "Experience Audi",
     links: [
-      'Models & Technology',
-      'Mobility & Trends',
-      'Inspiration & Lifestyle',
-      'Audi Sport',
+      "Models & Technology",
+      "Mobility & Trends",
+      "Inspiration & Lifestyle",
+      "Audi Sport",
     ],
   },
   {
     id: uuidv4(),
-    title: 'AUDI AG',
-    links: ['Strategy', 'Investor Relations', 'Sustainability', 'Career'],
+    title: "AUDI AG",
+    links: ["Strategy", "Investor Relations", "Sustainability", "Career"],
   },
-]
+];
 
-export default data
+export const longTexts = [
+  {
+    id: uuidv4(),
+    text: "* The specified fuel consumption and emission data have been determined according to the measurement procedures prescribed by law. Since 1st September 2017, certain new vehicles are already being type-approved according to the Worldwide Harmonized Light Vehicles Test Procedure (WLTP), a more realistic test procedure for measuring fuel consumption and CO2 emissions. Starting on September 1st 2018, the New European Driving Cycle (NEDC) will be replaced by the WLTP in stages. Owing to the more realistic test conditions, the fuel consumption and CO2 emissions measured according to the WLTP will, in many cases, be higher than those measured according to the NEDC. Therefore, the usage of CO2 emission values measured according to WLTP for vehicle taxation from 1st September 2018 on can cause changes in this regards as well. For further information on the differences between the WLTP and NEDC, please visit www.audi.de/wltp.",
+  },
+  {
+    id: uuidv4(),
+    text: "We are currently still required by law to state the NEDC figures. In the case of new vehicles which have been type-approved according to the WLTP, the NEDC figures are derived from the WLTP data. It is possible to specify the WLTP figures voluntarily in addition until such time as this is required by law. In cases where the NEDC figures are specified as value ranges, these do not refer to a particular individual vehicle and do not constitute part of the sales offering. They are intended exclusively as a means of comparison between different vehicle types. Additional equipment and accessories (e.g. add-on parts, different tire formats, etc.) may change the relevant vehicle parameters, such as weight, rolling resistance and aerodynamics, and, in conjunction with weather and traffic conditions and individual driving style, may affect fuel consumption, electrical power consumption, CO2 emissions and the performance figures for the vehicle.",
+  },
+  {
+    id: uuidv4(),
+    text: "Further information on official fuel consumption figures and the official specific CO2 emissions of new passenger cars can be found in the “Guide on the fuel economy, CO2 emissions and power consumption of new passenger car models”, which is available free of charge at all sales dealerships and from DAT Deutsche Automobil Treuhand GmbH, Hellmuth-Hirth-Str. 1, D-73760 Ostfildern, Germany and at www.dat.de.",
+  },
+];
+export default data;
